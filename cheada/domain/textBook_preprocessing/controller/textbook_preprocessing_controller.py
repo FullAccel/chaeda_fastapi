53c923a9c1afbe8b6c83bf4ef09f0e4403f4459c
sejoon00 <parksejoon313@gmail.com>
2024-04-29 15:49:38 +0900

    controller에서 s3utils 부르는 로직 제거

--- cheada/domain/textBook_preprocessing/controller/textbook_preprocessing_controller.py
+++ cheada/domain/textBook_preprocessing/controller/textbook_preprocessing_controller.py
@@ -1,7 +1,7 @@
 import threading
 from fastapi import APIRouter
 from pydantic import BaseModel
-from cheada.cloud_service_agent.s3 import s3_utils
+from cheada.domain.textBook_preprocessing.service import textbook_service
 
 
 class Data(BaseModel):
@@ -15,7 +15,7 @@ router = APIRouter()
 async def preprocessing(data: Data):
     fileName = data.fileName
     save_location = "globalUtils/temp_textbook_storage"
-    s3_utils.download_file_from_s3(filename=fileName, save_location=save_location)
+    textbook_service.download_textbook_from_s3(filename=fileName, file_location=save_location)
 
     #현재 spring 서버가 동기로 응답을 기다리고 있기 때문에 전처리하는 과정을 thread를 만들어 비동기로 처리한 후 spring에게
     # 바로 응답을 보내줘야합니다.
