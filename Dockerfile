cb72feb43b60ad18399a88cf6e27d04d98f3b3c7
seojmm <myungs8562@gmail.com>
2024-05-15 15:33:44 +0900

    docker test

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /code
 COPY requirements.txt ./
 
 #
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
 #
 COPY . .
--- cheada/db/crud.py
+++ cheada/db/crud.py
@@ -1,40 +1,3 @@
-# from sqlalchemy.orm import Session
-
-# from . import models, schemas
-
-
-# def get_user(db: Session, user_id: int):
-#     return db.query(models.User).filter(models.User.id == user_id).first()
-
-
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(models.User).filter(models.User.email == email).first()
-
-
-# def get_users(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.User).offset(skip).limit(limit).all()
-
-
-# def create_user(db: Session, user: schemas.UserCreate):
-#     fake_hashed_password = user.password + "notreallyhashed"
-#     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
-
-
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
-
 from sqlalchemy.orm import Session
 
 # from cheada_fastapi.cheada.domain.textBook_preprocessing.dto.ProblemInfoDto import ProblemInfoDto
@@ -74,12 +37,12 @@ def get_type_by_subject(db: Session, subject: str):
 	return db.query(models.MathProblemType).filter(models.MathProblemType.subject == subject).first()
 
 def create_type(db: Session, type: schemas.MathProblemTypeCreate):
-	# db_type = models.Type(
-	#     subject=type.subject,
-	#     chapter=type.chapter,
-	#     subconcept=type.subconcept
-	# )
-	db_type = models.MathProblemType(**type.model_dump())
+	db_type = models.Type(
+	    subject=type.subject,
+	    chapter=type.chapter,
+	    subconcept=type.subconcept
+	)
+	# db_type = models.MathProblemType(**type.model_dump())
 	db.add(db_type)
 	db.commit()
 	db.refresh(db_type)
