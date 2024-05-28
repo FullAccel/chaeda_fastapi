fa8ee46f8240bf0e21e0c5eec21a3015f815819b
seojmm <myungs8562@gmail.com>
2024-05-28 19:51:26 +0900

    Fix: review_note_maker object id changed

--- cheada/domain/review_note_maker/controller/review_note_maker_controller.py
+++ cheada/domain/review_note_maker/controller/review_note_maker_controller.py
@@ -16,12 +16,12 @@ class FileExtension(Enum):
 
 
 class ReviewNoteProblemInfo(BaseModel):
-    incorrect_date: str = Field(..., description='문제를 틀린 날짜', example='2024-05-22')
-    image_key: str = Field(..., description='이미지 파일의 고유 키 값', example='10a99bab-4940-48af-92e7-867a56d6ec79')
-    file_extension: FileExtension = Field(default=FileExtension.PNG, description='이미지 파일의 확장자')
+    incorrectDate: str = Field(..., description='문제를 틀린 날짜', example='2024-05-22')
+    imageKey: str = Field(..., description='이미지 파일의 고유 키 값', example='10a99bab-4940-48af-92e7-867a56d6ec79')
+    fileExtension: FileExtension = Field(default=FileExtension.PNG, description='이미지 파일의 확장자')
     answer: str = Field(..., description='문제의 정답', example='42')
-    textbook_name: str = Field(..., description='교재 이름', example='쎈 미적분')
-    problem_num: str = Field(..., description='문제 번호', example='101')
+    textbookName: str = Field(..., description='교재 이름', example='쎈 미적분')
+    problemNum: str = Field(..., description='문제 번호', example='101')
     chapter: ChapterEnum = Field(..., description='문제가 속한 챕터')
 
 
@@ -39,7 +39,7 @@ def reviewNoteMaker(data: ReviewNoteMakeRequest):
     fileName = data.filename
     for info in data.review_note_problem_info_list:
 
-        s3_problem_image_path = f"review_note_problem/{data.memberId}/{info.image_key}.png"
+        s3_problem_image_path = f"review_note_problem/{data.memberId}/{info.imageKey}.png"
 
         review_note_service.download_problem_image_from_s3(filename=s3_problem_image_path, file_location=temp_problem_storage)
 
